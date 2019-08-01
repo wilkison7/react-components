566dd11bc7de3c09546e6ddcd2c5e5a9ac7d324a
vincaslt <vincaslt@gmail.com>
2019-08-01 13:46:47 +0300

    Reset modals when navigating

--- components/modal/Content.js
+++ components/modal/Content.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { noop } from 'proton-shared/lib/helpers/function';
 
-const Content = ({ children, className = '', onSubmit, onReset, autoComplete = 'off', ...rest }) => {
+const Content = ({ children, className = '', onSubmit = noop, onReset = noop, autoComplete = 'off', ...rest }) => {
     const handleSubmit = (event) => {
         event.preventDefault();
         onSubmit(event);
@@ -24,7 +25,7 @@ Content.propTypes = {
     className: PropTypes.string,
     onSubmit: PropTypes.func,
     onReset: PropTypes.func,
-    autoComplete: PropTypes.string.isRequired
+    autoComplete: PropTypes.string
 };
 
 export default Content;
